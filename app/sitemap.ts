9dc6146c7c0d4629c9535798368a722758083252
phyothantzin <phyothantzin.w@gmail.com>
2024-05-20 15:14:31 +0630

    modify sitemap and add robots.txt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,17 @@
 import { MetadataRoute } from "next";
+import { createSlug } from "@/helper/slugify";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const res = await fetch(
+    `https://newapi.goldenyellowtravel.com/api/v1/tour/list`
+  );
+  const tours = await res.json();
+
+  const toursEntries = tours?.data.map((tour: any) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/tour/tour-detail/${createSlug(
+      tour?.name
+    )}`,
+  }));
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
@@ -17,5 +28,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/news`,
     },
+    ...toursEntries,
   ];
 }
